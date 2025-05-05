9d90a0f544dfd78152dfdbbc249ffb6486d0bed1
Daud Zoss <daud.zoss@philips.com>
2025-05-05 16:21:08 -0700

    rough startup screen verified on all platforms

---
+++ Makefile
@@ -0,0 +1,13 @@
+all : shiftlocks_p500.prg shiftlocks_c64.prg shiftlocks_c16.prg
+
+shiftlocks_p500.prg : main.asm p500/header.inc
+	rm -f header.inc; ln -s p500/header.inc; 64tass -a header.inc main.asm -L shiftlocks_p500.lst -o shiftlocks_p500.prg
+
+shiftlocks_c64.prg : main.asm c64/header.inc
+	rm -f header.inc; ln -s c64/header.inc; 64tass -a header.inc main.asm -L shiftlocks_c64.lst -o shiftlocks_c64.prg
+
+shiftlocks_c16.prg : main.asm c16/header.inc
+	rm -f header.inc; ln -s c16/header.inc; 64tass -a header.inc main.asm -L shiftlocks_c16.lst -o shiftlocks_c16.prg
+
+clean :
+	rm -f *.prg *.lst
---
+++ main.asm
@@ -0,0 +1,120 @@
+.if BASIC
+*	= BASIC+1
+.else
+*	= $0002+1
+COPIED2	= $0400
+	.word	(+), 3
+	.text	$81,$41,$b2,$30	; FOR A = 0
+	.text	$a4		; TO prefld-start
+	.text	format("%4d",pre_end-start)
+	.text	$3a,$dc,$30	; : BANK 0
+	.text	$3a,$42,$b2,$c2	; : B = PEEK
+	.text	$28		; ( start
+	.text	format("%2d",COPIED2)
+	.text	$aa,$41,$29,$3a	; + A ) :
+	.text	$dc,$31,$35,$3a	; BANK 1 5 :
+	.text	$97		; POKE start
+	.text 	format("%2d",COPIED2)
+	.text	$aa,$41,$2c,$42	; + A , B
+	.text	$3a,$82,$00	; : NEXT
++
+.endif
+
+	.word	(+), 2055
+	.text	$99,$22,$09,$8e	; PRINT " CHR$(9) CHR$(142)
+	.text	$08,$93,$13,$13	; CHR$(8) CHR$(147) CHR$(19) CHR$(19)
+topline	.text	"the crime scene     "
+	.text	"  shiftlock's office"
+	.text   "                 "
+	.null	"draw",$22
++	.word	(+), 2055
+	.text	$99,$3a,$99,$22
+	.text   "                 "
+	.null	"deck",$22
++	.word	(+), 2055
+	.text	$99,$22
+	.text	"                 "
+.if BASIC
+	.null	"   ",$6e,$22
+.else
+	.null	"   /",$22
+.endif
++	.word	(+), 2055
+	.text	$99,$22
+	.text	"                 "
+	.null	$12,"   ",$92,$22
++	.word	(+), 2055
+	.text	$99,$22
+	.text	"    threats      "
+	.null	$12,"   ",$92,$22
++	.word	(+), 2055
+	.text	$99,$22
+	.text	"                 "
+	.null	$12," ? ",$92,$22
++	.word	(+), 2055
+	.text	$99,$22
+	.text	" investigations  "
+	.null	$12,"   ",$92,$22
++	.word	(+), 2055
+	.text	$99,$22
+	.text	"                 "
+	.null	$12,"   ",$92,$22
++	.word	(+), 2055
+	.text	$99,$3a,$99,$3a,$99
+	.text	$3a,$99,$3a,$99,$3a
+	.text	$99,$22
+	.text	" ",$5e,"f1"
+	.text	" ",$5e,"f3"
+	.text	" ",$5e,"f5"
+	.null	" ",$5e,"f7",$22
++	.word	(+), 2055
+	.text	$99,$22
+	.text	"                 "
+.if BASIC
+	.null	$6e,$22
+.else
+	.null	"/",$22
+.endif
++	.word	(+), 2055
+	.text	$99,$22
+	.text	"                 "
+	.null	"dis-",$22
++	.word	(+), 2055
+	.text	$99,$22
+	.text	"                 "
+	.null	"card",$22,$3a,$99
++	.word	(+),2055
+	.text	$99,$22
+	.text	"                 "
+	.null	"pile",$22
++	.word	(+),2055
+	.text	$99,$22
+	.text	" f2",$c0," f4",$c0
+	.text	" f6",$c0," f8",$c0
+	.null	$22,$3a,$99
++	.word	(+),2055	
+	.text	$99,$22,"pairing "
+	.text	"   +       +    "
+	.null	"   +       +",$22
++	.word	(+),2055
+	.text	$99,$22
+	.text	" rules: invest",$c0
+	.text	" ",$5f,"threat"
+	.text	" ",$5f,"invest"
+	.null	" ex pile",$22,$3b
++	.word	(+),2055
+	.text	$9e
+	.null	format("%4d",main)
++	.word	0
+
+.if !BASIC
+*	= COPIED2
+.endif
+	
+start
+
+main
+	jsr	$ffe4
+	beq	main
+	rts
+pre_end
