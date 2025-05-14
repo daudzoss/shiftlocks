463fc071cae891d3c8fa094ee55cd67ace16b336
Daud Zoss <daud.zoss@philips.com>
2025-05-14 12:26:02 -0700

    gamewon() conditions for both cases

--- problem.asm
+++ problem.asm
@@ -6,5 +6,35 @@ tosolve	.byte	$01,$20,$03,$0f	; 1 CO
 	.byte	$40		; _
 +
 
-gamewon	lda	#0
-	rts
+gamewon	ldx	#1		;uint1_t gamewon(void) {
+	ldy	#0		;
+-	iny			; for (register uint8_t y = 1; y < 4; y++) {
+	lda	STACKHT+8-1,y	;
+	clc			;                                       
+	pha;1:current stack# 1~4;
+	tay			;  int8_t y_ = STACKHT[(8-1)+y]; // stacks 8~11
+	cpy	#7		;
+	bcc	+++++		;  if (y_ >= 7) { // enough to have 4x investig.
+-	lda	ODRAWER,x	;   for (register uint8_t x = 0; x < 15; x+=2) {
+	and	#$04		;    if (ODRAWER[(y*16 + x) | 1] & 0x04) // odds
+	bne	++++		;     break; // threat, skip stack!
+	inx			;
+	inx			;
+	dey			;
+	beq	+		;
+	dey			;    y_ -= 2; // skip intervening investig. card
+	bne	-		;    if (y_ <= 0) // confirmed a no-threat stack
++	lda	#1		;
+	pla;1->0		;     return 1; // won
+	rts			;   }
++	pla;1->0		;
+	tay			;
+	txa			;  }
+	and	#$f0		;
+	clc			;
+	adc	#$11		;
+	tax			;
+	and	#$40		;
+	bne	--		; }
+	lda	#0		; return 0;
+	rts			;} // gamewon()
--- valley.asm
+++ valley.asm
@@ -6,5 +6,16 @@ tosolve	.byte	$33,$20,$03,$0f	; 3 CO
 	.byte	$57		; @
 +
 
-gamewon	lda	#0
-	rts
+gamewon	ldy	#4		;uint1_t gamewon(void) {
+	ldx	#0		; uint2_t x;
+-	lda	STACKHT+8-1,y	; for (uint8_t y = 4; y >= 1; y--) {
+	cmp	#7		;
+	bcc	+		;  if (STACKHT[(8-1)+y] >= 7) // enough for 4x i
+	inx			;   x++;
++	dey			;
+	bne	-		; }
+	cpx	#3		;
+	bcc	+		;
+	ldy	#1		;
++	tya			; return (x >= 3);
+	rts			;} // gamewon()
