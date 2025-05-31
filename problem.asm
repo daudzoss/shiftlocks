addf8137af7664bed48f2d7c6b504410436d3dcc
Daud Zoss <daudzoss@gmail.com>
2025-05-31 15:24:52 -0700

    revised the tosolve message to be clearer about requiring at least 4 investigation cards and no threat cards

--- problem.asm
+++ problem.asm
@@ -1,9 +1,13 @@
 ;;;
 solvlen	.byte	(+)-tosolve
-tosolve	.byte	$01,$20,$03,$0f	; 1 CO
-	.byte	$0c,$20,$0f,$06	; L OF
-	.byte	$20,$30,$20,$5b	;  0 #
-	.byte	$40		; _
+tosolve	.byte	$34,$2d,$40,$57	; 4-_@
+	.byte	$2c,$30,$2d,$5b	; ,0-#
+	.byte	$40,$20,$03,$0f	; _ CO
+	.byte	$0c		; L
+;	.byte	$01,$20,$03,$0f	; 1 CO
+;	.byte	$0c,$20,$0f,$06	; L OF
+;	.byte	$20,$30,$20,$5b	;  0 #
+;	.byte	$40		; _
 +
 
 gamewon	ldx	#1		;uint1_t gamewon(void) {
